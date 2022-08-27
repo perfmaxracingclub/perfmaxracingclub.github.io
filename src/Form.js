31e342858e6fc3f3741ca35debdfa5aee1720fbc
perfmaxracingclub <perfmaxracingclub@gmail.com>
2022-08-26 21:10:08 -0400

    implemented table, not sure about the look though

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select'
-import Button from '@mui/material/Button'
-import { tableForget, fetchDistances, fetchBestTimesByCategory } from './tableForget.js'
+import Grid from '@mui/material/Grid'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
+
+
+import { fetchDistances, fetchBestTimesByCategory, fetchCategoryMetadata, generateDataTable } from './tableForget.js'
+
+
+
+
+const percentages = [
+    '100%', '95%', '90%', '85%', '80%', '75%', '70%', '65%', '60%', '55%', '50%'
+]
 
 
 const Form = () => {
@@ -17,73 +33,127 @@ const Form = () => {
 
     const distances = fetchDistances();
 
-
-
-    //console.log(tableForget, fetchDistances());
-
     function handleChangeDistance(event) {
         setDistance(event.target.value)
         setTimes(fetchBestTimesByCategory(event.target.value))
-
-        //console.log(times)
     }
 
     function handleChangeTime(event) {
         setTime(event.target.value)
-
     }
+
+    const categoryData = fetchCategoryMetadata(distance, time)
+    const rows = categoryData ? generateDataTable(categoryData.category) : []
 
     return (
         <div>
-            <form>
+            <Grid container spacing={2}>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={4}>
 
-                <Box sx={{ minWidth: 120 }}>
-                    <FormControl sx={{ m: 1, minWidth: 120 }}>
-                        <InputLabel id="distance-select-label">Distance</InputLabel>
-                        <Select
-                            labelId="distance-select-label"
-                            id="distance-select"
-                            value={distance}
-                            label="Distance"
-                            onChange={handleChangeDistance}
-                        >
-                            {distances.map((distance) => (
-                                <MenuItem
-                                    key={distance}
-                                    value={distance}
-                                >
-                                    {distance}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Box>
-                <FormControl sx={{ m: 1, minWidth: 120 }}>
-                    <InputLabel id="time-select-label">Time</InputLabel>
-                    <Select
-                        labelId="time-select-label"
-                        id="time-select"
-                        value={time}
-                        label="Time"
-                        onChange={handleChangeTime}
-                    >
-                        {times.map((time) => (
-                            <MenuItem
-                                key={time}
-                                value={time}
+                    <Box sx={{ minWidth: 120 }} display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        minHeight="12vh">
+
+                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel id="distance-select-label">Distance</InputLabel>
+                            <Select
+                                labelId="distance-select-label"
+                                id="distance-select"
+                                value={distance}
+                                label="Distance"
+                                onChange={handleChangeDistance}
                             >
-                                {time}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
+                                {distances.map((distance) => (
+                                    <MenuItem
+                                        key={distance}
+                                        value={distance}
+                                    >
+                                        {distance}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
 
-                <Button variant="contained">Submit</Button>
-            </form>
-        </div>
+                        <FormControl sx={{ m: 1, minWidth: 120 }}>
+                            <InputLabel id="time-select-label">Time</InputLabel>
+                            <Select
+                                labelId="time-select-label"
+                                id="time-select"
+                                value={time}
+                                label="Time"
+                                onChange={handleChangeTime}
+                            >
+                                {times.map((time) => (
+                                    <MenuItem
+                                        key={time}
+                                        value={time}
+                                    >
+                                        {time}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Grid>
+
+                <Grid item xs={4}></Grid>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={4}>
+                    <Box sx={{ minWidth: 120 }} display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        minHeight="4vh">
+                        <label>Category: {categoryData?.category}</label>
+                    </Box>
+                </Grid>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={8}>
+                    <TableContainer component={Paper}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Distance</TableCell>
+                                    {
+                                        percentages.map((col => (
+                                            <TableCell align="right" >{col}</TableCell>
+                                        )))
+                                    }
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                        key={row.distance}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.distance}
+                                        </TableCell>
+                                        <TableCell align="right">{row['100%']}</TableCell>
+                                        <TableCell align="right">{row['95%']}</TableCell>
+                                        <TableCell align="right">{row['90%']}</TableCell>
+                                        <TableCell align="right">{row['85%']}</TableCell>
+                                        <TableCell align="right">{row['80%']}</TableCell>
+                                        <TableCell align="right">{row['75%']}</TableCell>
+                                        <TableCell align="right">{row['70%']}</TableCell>
+                                        <TableCell align="right">{row['65%']}</TableCell>
+                                        <TableCell align="right">{row['60%']}</TableCell>
+                                        <TableCell align="right">{row['55%']}</TableCell>
+                                        <TableCell align="right">{row['50%']}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={2}></Grid>
+            </Grid>
+        </div >
     )
 
 }
-
 
 export default Form
--- src/tableForget.js
+++ src/tableForget.js
@@ -5,71 +5,77 @@ export const tableForget = {
         {
             category: 'A-1',
             bestTime: `00'10".65`,
-            timeByPercentages: [
-                { percentage: 100, time: `00'10".65` },
-                { percentage: 95, time: `00'11".21` },
-                { percentage: 90, time: `00'11".84` },
-                { percentage: 85, time: `00'12".54` },
-                { percentage: 80, time: `00'13".31` },
-                { percentage: 75, time: `00'14".20` },
-                { percentage: 70, time: `00'15".22` },
-                { percentage: 65, time: `00'16".39` },
-                { percentage: 60, time: `00'17".76` },
-                { percentage: 55, time: `00'19".37` },
-                { percentage: 50, time: `00'21".31` }
-            ]
+            timeByPercentages: {
+                '100%': `00'10".65`,
+                '95%': `00'11".21`,
+                '90%': `00'11".84`,
+                '85%': `00'12".54`,
+                '80%': `00'13".31`,
+                '75%': `00'14".20`,
+                '70%': `00'15".22`,
+                '65%': `00'16".39`,
+                '60%': `00'17".76`,
+                '55%': `00'19".37`,
+                '50%': `00'21".31`
+            }
         }, {
             category: 'A-2',
             bestTime: `00'10".71`,
-            timeByPercentages: [
-                { percentage: 100, time: `00'10".65` },
-                { percentage: 95, time: `00'11".21` },
-                { percentage: 90, time: `00'11".84` },
-                { percentage: 85, time: `00'12".54` },
-                { percentage: 80, time: `00'13".31` },
-                { percentage: 75, time: `00'14".20` },
-                { percentage: 70, time: `00'15".22` },
-                { percentage: 65, time: `00'16".39` },
-                { percentage: 60, time: `00'17".76` },
-                { percentage: 55, time: `00'19".37` },
-                { percentage: 50, time: `00'21".31` }
-            ],
+            timeByPercentages: {
+                '100%': `00'10".65`,
+                '95%': `00'11".21`,
+                '90%': `00'11".84`,
+                '85%': `00'12".54`,
+                '80%': `00'13".31`,
+                '75%': `00'14".20`,
+                '70%': `00'15".22`,
+                '65%': `00'16".39`,
+                '60%': `00'17".76`,
+                '55%': `00'19".37`,
+                '50%': `00'21".31`
+            },
         }],
     '120m': [
         {
             category: 'A-1',
             bestTime: `00'12".93`,
-            timeByPercentages: [
-                { percentage: 100, time: `00'10".65` },
-                { percentage: 95, time: `00'11".21` },
-                { percentage: 90, time: `00'11".84` },
-                { percentage: 85, time: `00'12".54` },
-                { percentage: 80, time: `00'13".31` },
-                { percentage: 75, time: `00'14".20` },
-                { percentage: 70, time: `00'15".22` },
-                { percentage: 65, time: `00'16".39` },
-                { percentage: 60, time: `00'17".76` },
-                { percentage: 55, time: `00'19".37` },
-                { percentage: 50, time: `00'21".31` }
-            ]
+            timeByPercentages: {
+                '100%': `00'10".65`,
+                '95%': `00'11".21`,
+                '90%': `00'11".84`,
+                '85%': `00'12".54`,
+                '80%': `00'13".31`,
+                '75%': `00'14".20`,
+                '70%': `00'15".22`,
+                '65%': `00'16".39`,
+                '60%': `00'17".76`,
+                '55%': `00'19".37`,
+                '50%': `00'21".31`
+            }
         }, {
             category: 'A-2',
             bestTime: `00'13".00`,
-            timeByPercentages: [
-                { percentage: 100, time: `00'10".65` },
-                { percentage: 95, time: `00'11".21` },
-                { percentage: 90, time: `00'11".84` },
-                { percentage: 85, time: `00'12".54` },
-                { percentage: 80, time: `00'13".31` },
-                { percentage: 75, time: `00'14".20` },
-                { percentage: 70, time: `00'15".22` },
-                { percentage: 65, time: `00'16".39` },
-                { percentage: 60, time: `00'17".76` },
-                { percentage: 55, time: `00'19".37` },
-                { percentage: 50, time: `00'21".31` }
-            ],
+            timeByPercentages: {
+                '100%': `00'10".65`,
+                '95%': `00'11".21`,
+                '90%': `00'11".84`,
+                '85%': `00'12".54`,
+                '80%': `00'13".31`,
+                '75%': `00'14".20`,
+                '70%': `00'15".22`,
+                '65%': `00'16".39`,
+                '60%': `00'17".76`,
+                '55%': `00'19".37`,
+                '50%': `00'21".31`
+            },
         }
-    ]
+    ],
+    '150m': [],
+    '200m': [],
+    '250m': [],
+    '300m': [],
+    '350m': [],
+    '400m': []
 }
 
 
@@ -84,6 +90,26 @@ export function fetchDistances() {
 
 export function fetchBestTimesByCategory(distance) {
     const distanceData = tableForget[distance];
-    console.log(distance, distanceData)
     return distanceData.map(data => data.bestTime);
+}
+
+
+export function fetchCategoryMetadata(distance, bestTime) {
+    return tableForget[distance].find(item => item.bestTime === bestTime)
+}
+
+export function generateDataTable(categoryName) {
+    let categories = [];
+    for (let distance in tableForget) {
+        let distanceCategory = tableForget[distance].find(item => item.category === categoryName);
+        distanceCategory = { ...distanceCategory, distance }
+        categories.push(distanceCategory);
+    }
+
+
+
+    return categories.map(({ distance, timeByPercentages }) => {
+        console.log(timeByPercentages)
+        return { ...timeByPercentages, distance }
+    });
 }
